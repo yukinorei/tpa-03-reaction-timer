2a2c4d646906bcf6f0f34b023b6533b80fa48517
yukinorei <>
2018-09-29 12:47:11 +0900

    添付コミット

--- public/js/components/ReactionTimerGame.js
+++ public/js/components/ReactionTimerGame.js
@@ -7,6 +7,8 @@ class ReactionTimerGame {
     this.view = null;
     this.activeCellRow = null;
     this.activeCellCol = null;
+    this.activeSecondCellRow = null;
+    this.activeSecondCellCol = null;
     this.currentStartTime = null;
     this.currentEndTime = null;
   }
@@ -19,20 +21,34 @@ class ReactionTimerGame {
   startCycle() {
     this.currentStartTime = new Date().getTime(); // milliseconds
     this.view.deactivateCell(this.activeCellRow, this.activeCellCol);
+    this.view.deactivateCell(this.activeSecondCellRow, this.activeSecondCellCol);
     this.triggerRandomCell();
   }
 
   triggerRandomCell() {
-    const randomRowIndex = getRandomInt(0, NUM_ROWS);
-    const randomColIndex = getRandomInt(0, NUM_COLS);
-    this.activeCellRow = randomRowIndex;
-    this.activeCellCol = randomColIndex;
-    this.view.activateCell(randomRowIndex, randomColIndex);
+    for (let i = 1; i < 3; i += 1) {
+      const randomRowIndex = getRandomInt(0, NUM_ROWS);
+      const randomColIndex = getRandomInt(0, NUM_COLS);
+      if (this.activeCellRow === null) {
+        this.activeCellRow = randomRowIndex;
+        this.activeCellCol = randomColIndex;
+        this.view.activateCell(randomRowIndex, randomColIndex);
+      }
+      this.activeSecondCellRow = randomRowIndex;
+      this.activeSecondCellCol = randomColIndex;
+      this.view.activateCell(randomRowIndex, randomColIndex);
+    }
   }
 
   handleActiveCellSelected() {
-    this.view.deactivateCell(this.activeCellRow, this.activeCellCol);
-    this.calculateTime();
+    if (this.activeCellRow) {
+      this.view.deactivateCell(this.activeCellRow, this.activeCellCol);
+      this.calculateTime();
+    }
+    if (this.activeSecondCellRow) {
+      this.view.deactivateCell(this.activeSecondCellRow, this.activeSecondCellCol);
+      this.calculateTime();
+    }
   }
 
   calculateTime() {
@@ -42,7 +58,6 @@ class ReactionTimerGame {
 
   init() {
     this.view = new ReactionTimerGridView();
-
     this.view.registerActiveCellSelectedCallback(this.handleActiveCellSelected.bind(this));
     this.view.registerRoundStartCallback(this.handleRoundStart.bind(this));
 
